77c46ca6b5d09e1d219fac48703d99a203a7f4d5
talishEG <talishkhaneg@gmail.com>
2024-08-03 15:30:40 +0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userRoutes = require('./userRoutes');
+const logRoutes = require('./logRoutes');
 
 router.use('/user', userRoutes);
 router.use('/log', logRoutes);
