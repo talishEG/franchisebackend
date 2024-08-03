12d081e2af61e7e1aa008ed1bd3dd37efddd2d68
talishEG <talishkhaneg@gmail.com>
2024-08-03 14:56:25 +0500

    Logger

--- routes/logRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const logsController = require("../controllers/logsController");
-
-router.post('/create', logsController.create);
-
-module.exports = router
